6a14246a78c0f301d65fe4331ff50497b8303ad6
Jial08 <1005699801@qq.com>
2017-06-30 17:55:28 +0800

    Fix: 解决文件唯一标识问题

--- js/file_manage.js
+++ js/file_manage.js
@@ -9,7 +9,8 @@ jQuery(function() {
         // 设置图片缩略图大小
         thumbnailWidth = 100 * ratio,
         thumbnailHeight = 100 * ratio,
-        uploader;
+        uploader,
+        filePath;
 
     uploader = WebUploader.create({
         // 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
@@ -34,12 +35,8 @@ jQuery(function() {
 
     // 当有文件被添加进队列的时候
     uploader.on('fileQueued', function(file) {
-        var fileType = file.type;
-        alert(fileType);
-        alert(fileType.indexOf("isdmage"))
-        if (fileType.indexOf('image/') != -1) {
-            alert(1)
-        }
+        // 为文件添加全局唯一标识
+        file.uuid = generateUUID();
         var $li = $('<div id="' + file.id + '" class="item">' +
                 '<img>' +
                 '<h4 class="info" style="display:inline-block;width:300px;">' + file.name + '</h4>' +
@@ -59,6 +56,24 @@ jQuery(function() {
 
             $img.attr('src', src);
         }, thumbnailWidth, thumbnailHeight);
+
+    });
+
+    // 分块儿上传前传额外参数，默认为get传送
+    uploader.on('uploadBeforeSend', function(block, data) {
+        debugger;
+        // 分块儿开始位置
+        data.start = block.start;
+        // 分块儿结束位置
+        data.end = block.end;
+        // 文件存储路径
+        data.filePath = filePath;
+        // 文件类型
+        data.fileType = block.file.ext;
+        // 文件唯一标识
+        data.uuid = block.file.uuid;
+        // 删除其他数据
+        // delete data.key;
     });
 
     // 文件上传过程中创建进度条实时显示。
@@ -87,20 +102,52 @@ jQuery(function() {
     uploader.on('uploadAccept', function(file, response) {
         if (response.success == 1) {
             // 通过return true或false来告诉组件，此文件上传成功或失败。
-            return response.msg;
+            filePath = response.filePath;
+            return true;
         } else {
             return false;
         }
     });
 
+    // 上传出错
     uploader.on('uploadError', function(file) {
         $('#' + file.id).find('p.state').text('上传出错');
     });
 
+    // 上传完成
     uploader.on('uploadComplete', function(file) {
+        debugger;
+        // 清除进度条
         $('#' + file.id).find('.progress').fadeOut();
+        // 数据库存储文件信息
+        var fileData = { "fileName": file.name, "fileSize": (file.size / 1024).toFixed(2) + "k", "objectId": file.uuid };
+        $.ajax({
+            url: '/file-manage/filemanage/saveFileInfo',
+            type: 'post',
+            /*
+             * 设置以json格式放到请求体中传送，更安全，传送的数据必须是字符串
+             * 如果不设置contentType，data直接{"a":"a"}传送，则
+             * 传送方式为Content-Type:application/x-www-form-urlencoded; charset=UTF-8，数据是在请求头中存放的
+             */
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(fileData),
+            success: function(data) {
+
+                if (data.success == 1) {
+                    if (data.result == false) {
+                        alert("文件保存出错，请稍后重试");
+                    }
+                } else {
+                    alert("文件保存出错，请稍后重试");
+                }
+            },
+            error: function(data) {
+                alert("文件保存出错，请稍后重试");
+            }
+        });
     });
 
+    // 暂停开始上传改变按钮显示
     uploader.on('all', function(type) {
         if (type === 'startUpload') {
             state = 'uploading';
@@ -117,6 +164,7 @@ jQuery(function() {
         }
     });
 
+    // 开始上传和暂停上传按钮
     $btn.on('click', function() {
         if (state === 'uploading') {
             uploader.stop(true);
@@ -126,6 +174,18 @@ jQuery(function() {
     });
 });
 
+/*
+ * js产生文件全局唯一标识，也是作为分块儿id使用的，也作为文件名和存储信息id使用
+ */
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+};
 
 /**
  * 添加额外参数的方法
