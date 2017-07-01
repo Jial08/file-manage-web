99884f12bfa5afb9c5afbbc95ff6e0d5675f8beb
Jial08 <1005699801@qq.com>
2017-07-01 20:04:48 +0800

    Feat: 完成秒传和断点续传功能

--- js/file_manage.js
+++ js/file_manage.js
@@ -10,7 +10,87 @@ jQuery(function() {
         thumbnailWidth = 100 * ratio,
         thumbnailHeight = 100 * ratio,
         uploader,
-        filePath;
+        chunkSize = 5 * 1024 * 1024;
+
+    // WebUploader的hook接口
+    WebUploader.Uploader.register({
+        // 在文件发送之前执行，用文件前5M的MD5和最后修改时间来判断文件唯一性
+        'before-send-file': 'preupload',
+        // 在文件分片（如果没有启用分片，整个文件被当成一个分片）后，上传之前执行，用于做断点续传
+        'before-send': 'checkchunk'
+    }, {
+        preupload: function(file) {
+            var me = this,
+                owner = this.owner,
+                server = me.options.server,
+                deferred = WebUploader.Deferred();
+
+            owner.md5File(file, 0, chunkSize)
+
+            // 如果读取出错了，则通过reject告诉webuploader文件上传出错。
+            .fail(function() {
+                deferred.reject();
+            })
+
+            // md5值计算完成
+            .then(function(md5) {
+                file.md5 = md5;
+                var fileData = { "md5": md5, "lastModifiedDate": file.lastModifiedDate, "fileName": file.name };
+                $.ajax({
+                    url: "/file-manage/filemanage/checkFile",
+                    type: "post",
+                    contentType: 'application/json; charset=utf-8',
+                    data: JSON.stringify(fileData),
+                    success: function(data) {
+                        if (data.result) {
+                            // 如果文件已上传则跳过
+                            owner.skipFile(file);
+                            $('#' + file.id).find('p.state').text('已上传');
+                        } else {
+                            // 否则webuploader接着往下走
+                            deferred.resolve();
+                        }
+                    },
+                    error: function(error) {
+                        deferred.resolve();
+                    }
+                });
+            });
+
+            return deferred.promise();
+        },
+        checkchunk: function(block) {
+            var me = this,
+                owner = this.owner,
+                deferred = WebUploader.Deferred();
+            owner.md5File(block.blob, block.start, block.end)
+                .fail(function() { // 如果读取出错了，则通过reject告诉webuploader文件上传出错。
+                    deferred.reject();
+                })
+                .then(function(md5) { // md5值计算完成
+                    block.fileMd5 = md5;
+                    $.ajax({
+                        url: "/file-manage/filemanage/checkChunk?fileMd5=" + md5 + "&chunk=" + block.chunk,
+                        type: "get",
+                        success: function(data) {
+                            if (data.success == 1) {
+                                // 如果分块儿已上传则跳过
+                                block.file.uuid = data.fileId;
+                                deferred.reject();
+                            } else {
+                                // 否则webuploader接着往下走
+                                deferred.resolve();
+                            }
+                        },
+                        error: function(error) {
+                            deferred.resolve();
+                        }
+                    });
+                });
+            return deferred.promise();
+        }
+    });
+
 
     uploader = WebUploader.create({
         // 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
@@ -24,6 +104,12 @@ jQuery(function() {
         pick: '#picker',
         // 开启分片上传。
         chunked: true,
+        // [可选] [默认值：5242880] 如果要分片，分多大一片？ 默认大小为5M.
+        chunkSize: chunkSize,
+        // [默认值：3] 上传并发数。允许同时最大上传进程数。
+        threads: 3,
+        //  [默认值：2] 如果某个分片由于网络问题出错，允许自动重传多少次？
+        chunkRetry: 2,
         // 指定Drag And Drop拖拽的容器，如果不指定，则不启动。
         dnd: document.body,
         // 是否禁掉整个页面的拖拽功能，如果不禁用，图片拖进来的时候会默认被浏览器打开，默认false。
@@ -56,22 +142,22 @@ jQuery(function() {
 
             $img.attr('src', src);
         }, thumbnailWidth, thumbnailHeight);
-
     });
 
-    // 分块儿上传前传额外参数，默认为get传送
+
+
+    // 分块儿上传前传额外参数
     uploader.on('uploadBeforeSend', function(block, data) {
-        debugger;
         // 分块儿开始位置
         data.start = block.start;
         // 分块儿结束位置
         data.end = block.end;
-        // 文件存储路径
-        data.filePath = filePath;
         // 文件类型
         data.fileType = block.file.ext;
         // 文件唯一标识
         data.uuid = block.file.uuid;
+        // 分块儿文件MD5
+        data.fileMd5 = block.fileMd5;
         // 删除其他数据
         // delete data.key;
     });
@@ -101,8 +187,6 @@ jQuery(function() {
     // 判断文件是否上传成功
     uploader.on('uploadAccept', function(file, response) {
         if (response.success == 1) {
-            // 通过return true或false来告诉组件，此文件上传成功或失败。
-            filePath = response.filePath;
             return true;
         } else {
             return false;
@@ -116,11 +200,10 @@ jQuery(function() {
 
     // 上传完成
     uploader.on('uploadComplete', function(file) {
-        debugger;
         // 清除进度条
         $('#' + file.id).find('.progress').fadeOut();
         // 数据库存储文件信息
-        var fileData = { "fileName": file.name, "fileSize": (file.size / 1024).toFixed(2) + "k", "objectId": file.uuid };
+        var fileData = { "fileName": file.name, "fileSize": (file.size / 1024).toFixed(2) + "k", "objectId": file.uuid, "lastModifiedDate": file.lastModifiedDate, "md5": file.md5 };
         $.ajax({
             url: '/file-manage/filemanage/saveFileInfo',
             type: 'post',
@@ -134,11 +217,9 @@ jQuery(function() {
             success: function(data) {
 
                 if (data.success == 1) {
-                    if (data.result == false) {
-                        alert("文件保存出错，请稍后重试");
+                    if (!data.result) {
+                        $('#' + file.id).find('p.state').text('文件保存出错，请稍后重试');
                     }
-                } else {
-                    alert("文件保存出错，请稍后重试");
                 }
             },
             error: function(data) {
@@ -167,7 +248,10 @@ jQuery(function() {
     // 开始上传和暂停上传按钮
     $btn.on('click', function() {
         if (state === 'uploading') {
-            uploader.stop(true);
+            /**
+             * uploader.stop();当前的上传继续，下个文件不开始上传
+             */
+            uploader.stop(true); // 当前上传直接中断
         } else {
             uploader.upload();
         }
